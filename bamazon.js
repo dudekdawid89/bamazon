8aee5599c11bfaa7be4cecc133f2fb860fbc441c
dudekdawid89 <dudekdawid89@gmail.com>
2019-07-17 23:40:38 -0500

    program have display table, questions, is connected and upadated with database, fully works

--- bamazon.js
+++ bamazon.js
@@ -1,37 +1,96 @@
 var mysql = require("mysql");
-// var inquirer = require("inquirer");
-// const cTable = require("console.table");
+var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "Dawid",
-    password: "",
+    password: "Element1234!",
     database: "bamazon"
 
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    customerTable()
+    customerTable();
+
+
 });
 
-function customerTable(){
-    inqurier.prompt([
-        {
-        name: "item_id",
-        type: "number",
-        message: "Please enter ID product of the product you would like to buy",
-        validate: function(value){
-            if (isNaN(value) === false){
-                return true;
-            }
-            return false;
-        }
-    },
-    {
 
-    }
-])
+function questions() {
+
+
+    inquirer.prompt([
+        {
+            name: "item_id",
+            type: "input",
+            message: "Please enter ID product of the product you would like to buy",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        ,
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many units of product you would like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (answer) {
+   
+        connection.query("SELECT * FROM products WHERE ?", [{ item_id : answer.item_id }], function (err, res) {
+            if (err) throw err
+            var inputID = answer.item_id;
+            var quantity = answer.quantity;
+            var actualQuantity = res[0].stock_quantity;
+            var remainingQuantity = actualQuantity - quantity
+            if(actualQuantity>answer.quantity){
+                connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?",[remainingQuantity, inputID], function(err, res){
+                    if(err) throw err
+                })
+            }
+            else{
+                console.log("Is not enough items, please consider you order");
+                questions() 
+            }
+            customerTableUpdated();
+            console.log("--------------------------------------");
+            console.log(
+                "Your order:", res[0].product_name, "\n",
+                "Order Quantity:", quantity,"\n",
+                "Total cost:", (quantity* res[0].price), "\n"
+            );
+            console.log("--------------------------------------");
+            connection.end();
+        })
+    })
+}
+
+function customerTable() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err
+        console.table(res);
+        questions();
+
+    })
+}
+
+function customerTableUpdated() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err
+        console.table(res);
+    })
 }
